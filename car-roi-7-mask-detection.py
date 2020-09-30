9a4a8c874fa177dae14d74d3dace66caac7a3ff3
Alexios Karadimos <karadalex@gmail.com>
2020-09-30 19:50:44 +0300

    prepare 7th program: same as 2nd and added Canny edge detection

--- car-roi-7-mask-detection.py
+++ car-roi-7-mask-detection.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from drawnow import drawnow, figure
 from processing import *
+from algorithms import algorithm2
 
 
 # Create a VideoCapture object and read from input file
@@ -11,30 +15,64 @@ cap = cv2.VideoCapture('april21.avi')
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
 
+fig = plt.figure()
+plt.ion() #Tell matplotlib you want interactive mode to plot live data
+
+# Initialize global variables
 prev_frame = []
+frame = []
+gray = []
+hist = []
+roi_mask = []
+
+
+def draw_fig():
+  global frame, gray, hist
+  if frame.any():
+    # convert to grayscale
+    # TODO: this needs to be replaced, because it is calculated twice in every frame!
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # show the plotting graph of an image 
+    hist = plt.hist(gray.ravel(), 256, [0,256], density=True)
+    plt.title('Histogram')
+    plt.show()
+
+
 # Read until video is completed
 while(cap.isOpened()):
   # Capture frame-by-frame
   ret, frame = cap.read()
   if ret == True:
 
-    # Step 1: Frame processing/improvement
-    frame = blurring(frame)
+    drawnow(draw_fig)
 
-    # Step 2: Edge detection
-    frame_edges = cv2.Canny(frame, 200, 200)
+    # Apply ROI detection algorithm
+    masked_frame, roi_mask, roi_mask1, roi_mask2, roi_mask3 = algorithm2(frame)
 
-    # Step 3: Shape description
+    # Edge detection on masked frame
+    frame_edges = cv2.Canny(masked_frame, 200, 200)
 
     # Display the resulting frame
     cv2.imshow('Original Video', frame)
-    cv2.imshow('Edges Video', frame_edges)
+    cv2.imshow('ROI Mask', roi_mask*255)
+    cv2.imshow('ROI Video', masked_frame)
+    cv2.imshow('Edges on ROI', frame_edges)
 
     # Keep previous frame
     prev_frame = frame
 
+    # Press S on keyboard to save images
+    key = cv2.waitKey(25)
+    if key == ord('s'):
+      cv2.imwrite('original2.png', frame)
+      cv2.imwrite('roi-mask2-1.png', roi_mask1*255)
+      cv2.imwrite('roi-mask2-2.png', roi_mask2*255)
+      cv2.imwrite('roi-mask2-3.png', roi_mask3*255)
+      cv2.imwrite('roi-mask2.png', roi_mask*255)
+      cv2.imwrite('masked_frame1.png', masked_frame)
     # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    if key == ord('q'):
       break
 
   # Break the loop
